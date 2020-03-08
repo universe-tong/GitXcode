cf5e0987df3ed693567a3aa539997eba46af0ffa
universe-tong <1394829564@qq.com>
2020-03-08 19:09:41 +0800

    在master分支下完成了功能4。

--- GitXocde/ViewController.swift
+++ GitXocde/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("添加了功能1")
-        
+        print("在master分支下完成了功能4。")
     }
 
 
