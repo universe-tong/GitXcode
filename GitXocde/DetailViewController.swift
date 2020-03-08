b6259f569be52f2de5598f84150aabea06fad312
universe-tong <1394829564@qq.com>
2020-03-08 18:08:42 +0800

    新添加了功能一。

---
+++ GitXocde/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  GitXocde
+//
+//  Created by lwnlwn987 on 2020/3/8.
+//  Copyright © 2020 lwn. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
